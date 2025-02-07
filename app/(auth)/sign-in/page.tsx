4c934ba0d98f757780d93367f098991d9d2b15a3
hishamrehab <hishamrehab30@gmail.com>
2025-02-07 06:15:54 +0200

    Athentication

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,10 +1,8 @@
-import React from 'react'
+import AuthForm from '@/components/AuthForm'
 
 const SignIn = () => {
   return (
-    <div>
-      Page
-    </div>
+   <AuthForm type="sign-in"/>
   )
 }
 
