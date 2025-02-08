4d5a058f6fab65244335b4b669600c588482972a
hishamrehab <hishamrehab30@gmail.com>
2025-02-08 17:10:45 +0200

    HomePage.tsx

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true, 
+  },
+  images: { unoptimized: true } ,
 };
 
 export default nextConfig;
+
